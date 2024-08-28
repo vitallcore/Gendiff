0af76215e928ae3e067b37bed4b8ac303a0b4280
vitallcore <vitaslipknot@gmail.com>
2024-08-28 19:38:44 +0600

    modified gendiff.py

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -7,7 +7,11 @@ def main():
     )
     parser.add_argument('first_file', type=str, help='First configuration file')
     parser.add_argument('second_file', type=str, help='Second configuration file')
+    parser.add_argument('-f', '--format', type=str, help='set format of output', default='plain')
+
     args = parser.parse_args()
+
+    print(f"Comparing '{args.first_file}' and '{args.second_file}' with output format: '{args.format}'")
 
 
 if __name__ == "__main__":
