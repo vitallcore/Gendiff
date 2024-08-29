2eee8acf783eedf79884d6d9f1cf3ca9b5f7f813
vitallcore <vitaslipknot@gmail.com>
2024-08-29 20:35:50 +0600

    update

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -7,8 +7,10 @@ def main():
         description="Compares two configuration files and shows a difference."
     )
     parser.add_argument('first_file', type=str, help='First configuration file')
-    parser.add_argument('second_file', type=str, help='Second configuration file')
-    parser.add_argument('-f', '--format', type=str, help='set format of output', default='plain')
+    parser.add_argument('second_file',
+                        type=str, help='Second configuration file')
+    parser.add_argument('-f', '--format',
+                        type=str, help='set format of output', default='plain')
 
     args = parser.parse_args()
 
