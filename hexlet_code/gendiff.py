579d02faeb9a5879db60e88ae886a5ecf18030e1
vitallcore <vitaslipknot@gmail.com>
2024-08-29 18:39:47 +0600

    corrected gendiff.py

--- hexlet_code/gendiff.py
+++ hexlet_code/gendiff.py
@@ -1,7 +1,7 @@
 import json
 
 
-def bool_to_str(value):
+def format_value(value):
     if isinstance(value, bool):
         return str(value).lower()
     return value
@@ -18,13 +18,13 @@ def generate_diff(file_path1, file_path2):
 
     for key in all_keys:
         if key not in data2:
-            diff.append(f"  - {key}: {data1[key]}")
+            diff.append(f"  - {key}: {format_value(data1[key])}")
         elif key not in data1:
-            diff.append(f"  + {key}: {data2[key]}")
+            diff.append(f"  + {key}: {format_value(data2[key])}")
         elif data1[key] != data2[key]:
-            diff.append(f"  - {key}: {data1[key]}")
-            diff.append(f"  + {key}: {data2[key]}")
+            diff.append(f"  - {key}: {format_value(data1[key])}")
+            diff.append(f"  + {key}: {format_value(data2[key])}")
         else:
-            diff.append(f"    {key}: {data1[key]}")
+            diff.append(f"    {key}: {format_value(data1[key])}")
 
     return f"{{\n{'\n'.join(diff)}\n}}" if diff else "{}"
