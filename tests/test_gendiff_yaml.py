84d50b2b59c4bc7c40400099ec6a1ad53e55a577
vitallcore <vitaslipknot@gmail.com>
2024-09-06 18:41:11 +0600

    update

--- tests/test_gendiff_yaml.py
+++ tests/test_gendiff_yaml.py
@@ -22,54 +22,6 @@ def file4_path():
     return 'tests/fixtures/file4.yml'
 
 
-def test_generate_diff_stylish(file1_path, file2_path):
-    expected = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-    key5: value5
-}
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-    key: value
-}
-      + nest: str
-    }
-  - group2: {
-    abc: 12345
-    deep: {
-        id: 45
-    }
-}
-  + group3: {
-    deep: {
-        id: {
-            number: 45
-        }
-    }
-    fee: 100500
-}
-}"""
-    assert generate_diff(file1_path, file2_path) == expected
-
-
 def test_generate_diff(file3_path, file4_path):
     expected = """{
   - follow: false
